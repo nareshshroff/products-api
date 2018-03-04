832b6a0b87523356b93e081ea063dc77c25e23ee
nareshshroff <gangleader@gmail.com>
2018-03-04 18:27:35 -0500

    updated

--- db/dbserver.js
+++ db/dbserver.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/product');
+mongoose.connect('mongodb://mongodb:27017/product');
 
 module.export = {mongoose};
